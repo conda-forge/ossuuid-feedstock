3d17d0342cd4dc13645635b739ff9a66b86d8573
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2022-12-13 10:47:32 +0100

    Set cached va_copy info

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,10 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 && $target_platform == "osx-arm64" ]]; then
+    export ac_cv_va_copy=C99
+fi
+
 ./configure  --prefix=$PREFIX
 make -j
 make install
